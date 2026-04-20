19c147f114aa45b9d8aaa83a0980c20e8f6da872
Abdelaziz Yassin <zezo56ps4@gmail.com>
2026-04-20 14:48:17 +0200

    Create database_exploration.sql

---
+++ scripts/database_exploration.sql
@@ -0,0 +1,16 @@
+-- ==============================================================
+-- Database Exploration Script
+-- ==============================================================
+-- This script is used to explore the structure of the database.
+-- It retrieves a list of all tables available in the database
+-- and inspects the columns of a specific table (dim_customers).
+-- Useful for understanding database schema and metadata.
+
+-- Explore All Objects in the Database
+SELECT * FROM INFORMATION_SCHEMA.TABLES;
+
+-- Explore All Columns in the Database
+SELECT * FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_NAME = 'dim_customers';
+
+-- 
